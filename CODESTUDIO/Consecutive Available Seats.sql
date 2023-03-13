6dbb4f6092e8e62f1813e1ef0f19c572d6e989cb
Kartik Dhari <118604310+kartikdhari15@users.noreply.github.com>
2023-03-13 23:38:15 +0530

    Add files via upload

---
+++ CODESTUDIO/Consecutive Available Seats.sql
@@ -0,0 +1,12 @@
+select distinct(a.seat_id)
+from cinema a
+join cinema b on abs(a.seat_id-b.seat_id)=1
+and a.free=true and b.free=true
+order by a.seat_id;
+
+ seat_id 
+---------
+       3
+       4
+(2 rows)
+
