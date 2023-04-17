2101fa9f8165405091916236b26a11d5586c550b
Kartik Dhari <118604310+kartikdhari15@users.noreply.github.com>
2023-04-18 01:41:23 +0530

    Add files via upload

---
+++ CODESTUDIO/Students DB.sql
@@ -0,0 +1,12 @@
+insert into students values(3,'Kim','F'),(4,'Molina','F'),(5,'Dev','M');
+select * from students;
+
+INSERT 0 3
+ id |  name  | gender 
+----+--------+--------
+  1 | Ryan   | M
+  2 | Joanna | F
+  3 | Kim    | F
+  4 | Molina | F
+  5 | Dev    | M
+(5 rows)
