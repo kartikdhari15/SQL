3dfe646ac87bccc71e26c03ae5d8a2c48332aae9
Kartik Dhari <118604310+kartikdhari15@users.noreply.github.com>
2023-04-18 17:46:17 +0530

    Add files via upload

---
+++ CODESTUDIO/Marvel Cities.sql
@@ -0,0 +1,9 @@
+select * from CITY where Population>100000;
+
+  id  |   name   | countrycode | population 
+------+----------+-------------+------------
+ 3878 | Wakanda  | Marv        |     202705
+ 3981 | Titan    | Marv        |     212705
+ 3667 | Starlord | Marv        |     122705
+  375 | Agamato  | Marv        |    1000000
+(4 rows)
